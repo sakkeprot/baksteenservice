020c54bc9465433ed2fe0784723f2af19666a0de
sakkeprot <65361518+sakkeprot@users.noreply.github.com>
2026-02-27 21:57:01 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -5,6 +5,13 @@ baksteenservice - secrets.py
 # DeepSeek API key — replace with your real key
 DEEPSEEK_API_KEY = ""
 # OpenRouteService — https://openrouteservice.org/dev/#/home  (free, 2000 req/day)
-ORS_API_KEY = ""
+#ORS_API_KEY = "" #word niet meer gebruikt
+
 # WeatherApi — https://www.weatherapi.com/api  (free, 1000000 calls/month)
 OWM_API_KEY = ""
+# Delijn.be — De lijn API  (free, quasi onbeperkt)
+#DELIJN_API_KEY = "" #word niet meer gebruikt
+
+#STIB_API_KEY = "" #word niet meer gebruikt
+
+GOOGLE_MAPS_API_KEY = ""
